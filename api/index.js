519cff1f772f264fa6d17b5892a439f7ad369192
Eyhannn <castlegithub08@gmail.com>
2024-03-26 10:04:22 +0800

    isaa

--- api/index.js
+++ api/index.js
@@ -89,7 +89,7 @@ app.use("/api/messages", authMessages);
 //   console.log("API working");
 // });
 
-const server = app.listen(8800, () => {
+const server = app.listen( () => {
   console.log("API working on port 8800");
 });
 
