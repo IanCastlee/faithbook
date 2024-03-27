30016a3b1c5b659f3f833fc80acea6c25946cdcf
Eyhannn <castlegithub08@gmail.com>
2024-03-27 13:26:38 +0800

    year6

--- api/index.js
+++ api/index.js
@@ -46,7 +46,7 @@ app.use(cookieParser());
 
 //
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+  destination: function (req, file, cb) { 
     cb(null, "../client/public/upload");
   },
   filename: function (req, file, cb) {
--- client/src/pages/register/Register_2.jsx
+++ client/src/pages/register/Register_2.jsx
@@ -52,10 +52,7 @@ const Register_2 = () => {
       return;
     } else {
       try {
-        await axios.post(
-          "https://faithbook-api-five.vercel.app/api/auth/register",
-          inputs
-        );
+        await axios.post("api/auth/register", inputs);
         setSuccess("Successfully registered!");
         setErr(null);
       } catch (err) {
