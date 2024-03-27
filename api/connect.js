1a338c0bd2c114b3a791df3ff070f702a2077e92
Eyhannn <castlegithub08@gmail.com>
2024-03-27 12:14:14 +0800

    year1

--- api/connect.js
+++ api/connect.js
@@ -3,8 +3,8 @@
 import mysql from "mysql";
 
 export const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "faithbook",
+  host: "bmmhfglodorqtbmwpuvz-mysql.services.clever-cloud.com",
+  user: "urfouzeokb5qnv1t",
+  password: "AQ4JNAZjZzcLEct9upsr",
+  database: "bmmhfglodorqtbmwpuvz",
 });
--- api/index.js
+++ api/index.js
@@ -1,101 +1,7 @@
-// import Express from "express";
+import express from "express";
 
-// const app = Express();
+const app = express();
 
-// import userRoutes from "./routes/users.js";
-// import postRoutes from "./routes/posts.js";
-// import commentRoutes from "./routes/comments.js";
-// import likeRoutes from "./routes/likes.js";
-// import authRoutes from "./routes/auth.js";
-// import storiesRoutes from "./routes/stories.js";
-// import userPostsRoutes from "./routes/userPosts.js";
-// import relationshipRoutes from "./routes/relationships.js";
-// import authUserVerseRoutes from "./routes/userVerse.js";
-// import authAllVerseRoutes from "./routes/allVerses.js";
-// import authAFrRoutes from "./routes/friends.js";
-// import authMessages from "./routes/messages.js";
-
-// import cookieParser from "cookie-parser";
-// import cors from "cors";
-// import multer from "multer";
-
-// import { Server } from "socket.io";
-
-// const io = new Server({
-//   cors: {
-//     origin: "http://localhost:5173",
-//     methods: ["GET", "POST"],
-//   }
-// });
-
-// // io.on("connection", (socket) => {
-// //   console.log(`Someone has connected ${socket.id}`)
-// //   socket.on("disconnect", () => {
-// //     console.log(`${socket.id} : disconnected`)
-// //   })
-// // });
-
-
-// //middlewares
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Credentials", true);
-//   next();
-// });
-// app.use(Express.json());
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-   
-//   })
-// );
-// app.use(cookieParser());
-
-// //
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, "../client/public/upload");
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, Date.now() + file.originalname);
-//   },
-// });
-
-
-
-// //upload file
-// const upload = multer({ storage: storage });
-// app.post("/api/upload", upload.single("file"), (req, res) => {
-//   const file = req.file;
-//   res.status(200).json(file.filename);
-// });
-
-
-// app.use("/api/users", userRoutes);
-// app.use("/api/posts", postRoutes);
-// app.use("/api/comments", commentRoutes);
-// app.use("/api/likes", likeRoutes);
-// app.use("/api/auth", authRoutes);
-// app.use("/api/stories", storiesRoutes);
-// app.use("/api/userPosts", userPostsRoutes);
-// app.use("/api/relationships", relationshipRoutes);
-// app.use("/api/userVerse", authUserVerseRoutes);
-// app.use("/api/allVerse", authAllVerseRoutes);
-// app.use("/api/friendReqs", authAFrRoutes);
-// app.use("/api/messages", authMessages);
-
-
-
-// // app.listen(8800, () => {
-// //   console.log("API working");
-// // });
-
-// const server = app.listen(8800, () => {
-//   console.log("API working on port 8800");
-// });
-
-// io.attach(server);
-
-import Express from "express";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import commentRoutes from "./routes/comments.js";
@@ -108,42 +14,37 @@ import authUserVerseRoutes from "./routes/userVerse.js";
 import authAllVerseRoutes from "./routes/allVerses.js";
 import authAFrRoutes from "./routes/friends.js";
 import authMessages from "./routes/messages.js";
+
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import multer from "multer";
-import { Server } from "socket.io";
 
-const app = Express();
+import { Server } from "socket.io";
 
 const io = new Server({
   cors: {
     origin: "http://localhost:5173",
     methods: ["GET", "POST"],
-    credentials: true, // Allow credentials
   }
 });
 
-//middlewares
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://faithbook-client.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true'); // Allow credentials
-  next();
-});
+// io.on("connection", (socket) => {
+//   console.log(`Someone has connected ${socket.id}`)
+//   socket.on("disconnect", () => {
+//     console.log(`${socket.id} : disconnected`)
+//   })
+// });
 
-app.use(Express.json());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true, // Allow credentials
-  })
-);
+
+//middlewares
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true // Allow sending cookies from frontend
+}));
+app.use(express.json());
 app.use(cookieParser());
 
-// Other middleware and route setup...
-
-
+//
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "../client/public/upload");
@@ -153,12 +54,15 @@ const storage = multer.diskStorage({
   },
 });
 
+
+
 //upload file
 const upload = multer({ storage: storage });
 app.post("/api/upload", upload.single("file"), (req, res) => {
   const file = req.file;
   res.status(200).json(file.filename);
 });
+
 
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
@@ -172,6 +76,12 @@ app.use("/api/userVerse", authUserVerseRoutes);
 app.use("/api/allVerse", authAllVerseRoutes);
 app.use("/api/friendReqs", authAFrRoutes);
 app.use("/api/messages", authMessages);
+
+
+
+// app.listen(8800, () => {
+//   console.log("API working");
+// });
 
 const server = app.listen(8800, () => {
   console.log("API working on port 8800");
